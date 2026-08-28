f0e8844529e644638562212121d5ac88b4d085a8
vamshi <kanavenavamshi1414@gmail.com>
2026-08-28 22:38:02 +0530

    Update Terraform backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "kanavenavamshi-terraform-state-364410975057"
+    bucket       = "kanavenavamshi-terraformstate"
     key          = "cicd-eks-rds/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
